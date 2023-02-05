01bd328cf8f0b5a8bcb46fb4af5edb5a710fc921
cfdebug <rfadventure22@gmail.com>
2023-02-05 02:42:41 -0600

    Completed Last Activity and Cleaned Up/Compared to Solution

--- petfax/__init__.py
+++ petfax/__init__.py
@@ -1,12 +1,22 @@
+# config                    
 from flask import Flask
+from flask_migrate import Migrate 
 
+# factory
 def create_app():
     app = Flask(__name__)
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost:5432/PetFax'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False     
+    
+    from . import models 
+    models.db.init_app(app)
+    migrate = Migrate(app, models.db)
 
     @app.route('/')
     def index(): 
         return 'Hello, PetFax!'
-
+ 
     from . import pet 
     app.register_blueprint(pet.bp)
 
